2d57e9b967ccbb8721090e23072b277207003396
Kimiega <dmbelitmo@mail.ru>
2024-03-02 20:48:17 +0300

    init commit

---
+++ src/main/kotlin/com/kimiega/onlineshop/BlpsOnlineShopApplication.kt
@@ -0,0 +1,11 @@
+package com.kimiega.onlineshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlpsOnlineShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlpsOnlineShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/kimiega/onlineshop/BlpsOnlineShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kimiega.onlineshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlpsOnlineShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
