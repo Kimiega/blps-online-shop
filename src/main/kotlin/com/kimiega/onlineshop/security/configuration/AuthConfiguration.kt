a427653906705aee779a1c3c821bec3423818794
Kimiega <dmbelitmo@mail.ru>
2024-05-20 20:06:33 +0300

    move config classes

--- src/main/kotlin/com/kimiega/onlineshop/security/configuration/AuthConfiguration.kt
+++ src/main/kotlin/com/kimiega/onlineshop/security/configuration/AuthConfiguration.kt
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.info.License
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -21,15 +20,12 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.AuthenticationEntryPoint
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
 @EnableConfigurationProperties(JWTProperties::class)
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-class Configuration {
+class AuthConfiguration {
     @Bean
     fun userDetailsService(
         userService: UserService,
@@ -54,34 +50,4 @@ class Configuration {
     fun authenticationEntryPoint(): AuthenticationEntryPoint {
         return CustomAuthenticationEntryPoint()
     }
-
-    @Bean
-    fun openAPI(): OpenAPI {
-        return OpenAPI().addSecurityItem(
-            SecurityRequirement().addList
-                ("Bearer Authentication")
-        )
-            .components(Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
-            .info(
-                Info().title("My REST API")
-                    .description("BLPS WEB MARKET.")
-                    .version("1.0").contact(
-                        Contact().name("Dmitry Belyakov")
-                    )
-            )
-    }
-    private fun createAPIKeyScheme(): SecurityScheme {
-        return SecurityScheme().type(SecurityScheme.Type.HTTP)
-            .bearerFormat("JWT")
-            .scheme("bearer")
-    }
 }
-
-@Configuration
-@EnableWebMvc
-class WebConfig : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-    }
-}
-
